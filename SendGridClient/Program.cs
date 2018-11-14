5b84fd4ee603306b99e535775bdb58bb086f1b14
Troy Reeves <troyreeves2@gmail.com>
2018-11-13 23:37:58 -0600

    Implemented sendgrid through the use of a library.

---
+++ SendGridClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using dotenv.net;
+using SendGridLib;
+
+namespace SendGridClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DotEnv.Config();
+            EmailConf converter = new EmailConf();
+            converter.VerifyEmail("");
+
+        }
+}
+}
