86a6506c22fb5e9068d89465ad08ca70579b0dee
Jacob Vengen <jivengen1@buffs.wtamu.edu>
2018-11-16 15:00:13 -0600

    User.cs

---
+++ PRPCRepository/Models/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PRPCRepository.Models
+    {
+        public class User
+        {
+            public int UserID {get; set;}
+            public string RequestId { get; set; }
+            public string firstName {get; set;}
+            public string lastName {get; set;}
+            public string password {get; set;}
+            public string email {get; set;}
+        }
+}
